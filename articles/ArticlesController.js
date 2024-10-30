b23bf787ec5dd844b5b521cc3a06f7ad34d30b11
cleber <cleber.risu@outlook.com>
2024-10-30 15:59:04 -0300

    add edit article

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -52,4 +52,46 @@ router.post('/articles/delete', (req, res) => {
   }
 })
 
+router.get('/admin/articles/edit/:id', (req, res) => {
+  const id = req.params.id
+  Article.findByPk(id).then(article => {
+    if (article) {
+
+      Category.findAll().then(categories => {
+        res.render('admin/articles/edit', { article, categories })
+      })
+
+    } else {
+      res.redirect('/admin/articles')
+    }
+  }).catch(err => {
+    console.log(err);
+    res.redirect('/admin/articles')
+  })
+
+})
+
+router.post('/articles/update', (req, res) => {
+  const id = req.body.id
+  const title = req.body.title
+  const body = req.body.body
+  const category = req.body.category
+
+  Article.update({ 
+    title: title, 
+    slug: slugify(title), 
+    body: body,
+    categoryId: category
+  }, {
+    where: {
+      id: id
+    }
+  }).then(() => {
+    res.redirect('/admin/articles')
+  }).catch(err => {
+    console.log(err);
+    res.redirect('/')
+  })
+})
+
 module.exports = router
